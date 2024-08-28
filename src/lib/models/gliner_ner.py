ca3d214f24915d4856c89957be87eb88f65e890d
ivan <ivanhernandezroldan1@gmail.com>
2024-08-28 19:53:52 +0200

    Finish preprocessing dataset

--- src/lib/models/gliner_ner.py
+++ src/lib/models/gliner_ner.py
@@ -1,3 +1,26 @@
+from gliner import GLiNER
+
+
+# Initialize GLiNER
+def initialize_gliner():
+    return GLiNER.from_pretrained("urchade/gliner_multiv2.1")
+
+
+model = initialize_gliner()
+
+# Labels for entity prediction
+LABELS = [
+    "Adverse drug reactions",
+    "Diseases or medical conditions",
+    "Medications",
+    "Clinical findings",
+    "Symptoms experienced by patients",
+]
+
+
 def apply_ner_to_text(text):
-    # LLamar a GLiNER
-    return text + "\nHola soy un modelo de NER llamado GLiNER"
+    entities = model.predict_entities(text, LABELS, threshold=0.5)
+    ner_text = ""
+    for entity in entities:
+        ner_text += f"{entity['text']} => {entity['label']}\n"
+    return ner_text
